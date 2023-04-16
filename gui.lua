762cf93fd9ffde8e59a0e9859825ff3ba9c97284
SubnauticaLaserMain <123651736+SubnauticaLaserMain@users.noreply.github.com>
2023-04-16 19:04:28 +0200

    Add files via upload

---
+++ gui.lua
@@ -0,0 +1,266 @@
+if game.PlaceId == 4623386862 or game.PlaceId == 5661005779 then
+local library = loadstring(game:GetObjects("rbxassetid://7657867786")[1].Source)()
+local Wait = library.subs.Wait
+
+local JuliansWorld = library:CreateWindow({
+Name = "Julians Exploit -- Piggy",
+Themeable = {
+Info = "I made this own my own but i got help by Pepise"
+}
+})
+
+local MainTab = JuliansWorld:CreateTab({
+Name = "Main"
+})
+
+local ScriptSection = MainTab:CreateSection({
+Name = "Scripts"
+})
+
+ScriptSection:AddButton({
+    Name = 'Vynixius',
+    Callback = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Piggy/Loader.lua"))()
+end
+})
+
+ScriptSection:AddButton({
+    Name = 'rb bit',
+    Callback = function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/tip52/useless-projects/main/piggy%20rb%20battles%20bit'))()
+end
+})
+
+local MicSection = MainTab:CreateSection({
+Name = "Mic"
+})
+
+MicSection:AddButton({
+    Name = 'Infinite Yeld',
+    Callback = function()
+        loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+    end
+})
+
+MicSection:AddButton({
+    Name = 'Dark Dex V3',
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua", true))()
+    end
+})
+
+MicSection:AddButton({
+    Name = 'Remote Spy',
+    Callback = function()
+        loadstring(game:HttpGet("https://github.com/exxtremestuffs/SimpleSpySource/raw/master/SimpleSpy.lua"),true)()
+    end
+})
+
+elseif game.PlaceId == 147848991 then
+local library = loadstring(game:GetObjects("rbxassetid://7657867786")[1].Source)()
+local Wait = library.subs.Wait
+
+local JuliansWorld = library:CreateWindow({
+Name = "Julians Exploit -- Be A Parkour Ninja",
+Themeable = {
+Info = "I made this own my own but i got help by Pepise"
+}
+})
+
+local MainTab = JuliansWorld:CreateTab({
+Name = "Main"
+})
+
+local ScriptSection = MainTab:CreateSection({
+Name = "Scripts"
+})
+
+ScriptSection:AddButton({
+    Name = 'Be A Parkour Ninja Hit box',
+    Callback = function()
+    loadstring(game:HttpGet("https://rawscripts.net/raw/Be-A-Parkour-Ninja-Be-A-Parkour-Ninja-Hit-box-9855",true))()
+end
+})
+
+ScriptSection:AddButton({
+    Name = 'Blind Everyone lol',
+    Callback = function()
+        loadstring(game:HttpGet("https://rawscripts.net/raw/Be-A-Parkour-Ninja-Blind-Everyone-lol-8984",true))()
+    end
+})
+
+ScriptSection:AddButton({
+    Name = 'Infinie Jump',
+    Callback = function()
+        loadstring(game:HttpGet("https://rawscripts.net/raw/Be-A-Parkour-Ninja-simple-Infinite-jump-script-9853",true))()
+    end
+})
+
+local MicSection = MainTab:CreateSection({
+Name = "Mic"
+})
+
+MicSection:AddButton({
+    Name = 'Infinite Yeld',
+    Callback = function()
+        loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+    end
+})
+
+MicSection:AddButton({
+    Name = 'Dark Dex V3',
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua", true))()
+    end
+})
+
+MicSection:AddButton({
+    Name = 'Remote Spy',
+    Callback = function()
+        loadstring(game:HttpGet("https://github.com/exxtremestuffs/SimpleSpySource/raw/master/SimpleSpy.lua"),true)()
+    end
+})
+elseif game.PlaceId == 3056526277 then
+local library = loadstring(game:GetObjects("rbxassetid://7657867786")[1].Source)()
+local Wait = library.subs.Wait
+
+local JuliansWorld = library:CreateWindow({
+Name = "Julians Exploit -- Build and Survive!",
+Themeable = {
+Info = "I made this own my own but i got help by Pepise"
+}
+})
+
+local MainTab = JuliansWorld:CreateTab({
+Name = "Main"
+})
+
+local ScriptSection = MainTab:CreateSection({
+Name = "Scripts"
+})
+
+ScriptSection:AddButton({
+    Name = 'a dead game by money hungry preston bulid survive',
+    Callback = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraIsMe/ToraIsMe/main/0BuildandSurvive", true))()
+end
+})
+
+local MicSection = MainTab:CreateSection({
+Name = "Mic"
+})
+
+MicSection:AddButton({
+    Name = 'Infinite Yeld',
+    Callback = function()
+        loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+    end
+})
+
+MicSection:AddButton({
+    Name = 'Dark Dex V3',
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua", true))()
+    end
+})
+
+MicSection:AddButton({
+    Name = 'Remote Spy',
+    Callback = function()
+        loadstring(game:HttpGet("https://github.com/exxtremestuffs/SimpleSpySource/raw/master/SimpleSpy.lua"),true)()
+    end
+})
+elseif game.PlaceId == 6516141723 or game.PlaceId == 6839171747 then
+local library = loadstring(game:GetObjects("rbxassetid://7657867786")[1].Source)()
+local Wait = library.subs.Wait
+
+local JuliansWorld = library:CreateWindow({
+Name = "Julians Exploit -- DOORS 👁️",
+Themeable = {
+Info = "I made this own my own but i got help by Pepise"
+}
+})
+
+local MainTab = JuliansWorld:CreateTab({
+Name = "Main"
+})
+
+local ScriptSection = MainTab:CreateSection({
+Name = "Scripts"
+})
+
+ScriptSection:AddButton({
+    Name = 'anticheat bypass',
+    Callback = function()
+    loadstring(game:HttpGet("https://rawscripts.net/raw/DOORS-Doors-bypass-9995",true))()
+end
+})
+
+local BasicSection = MainTab:CreateSection({
+    Name = "Basic"
+})
+
+BasicSection:AddButton({
+    Name = 'Revive',
+    Callback = function()
+    game.ReplicatedStorage.Bricks.Revive:FireServer()
+end
+})
+
+BasicSection:AddButton({
+    Name = 'PlayAgain',
+    Callback = function()
+        game.ReplicatedStorage.Bricks.PlayAgain:FireServer()
+    end
+})
+
+ScriptSection:AddButton({
+    Name = 'MSDOORS',
+    Callback = function()
+        loadstring(game:HttpGet(("https://raw.githubusercontent.com/mstudio45/MSDOORS/main/MSDOORS.lua"),true))()
+    end
+})
+
+ScriptSection:AddButton({
+    Name = 'Entity Spawner',
+    Callback = function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/plamen6789/UtilitiesHub/main/UtilitiesGUI'))()
+    end
+})
+
+local MobileScriptSection = MainTab:CreateSection({
+    Name = 'MobileScripts'
+})
+
+MobileScriptSection:AddButton({
+    Name = 'Nerd Hub V3',
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/MrNeRD0/Doors-Hack/main/DoorsV3_ByNerd.lua"))()
+    end
+})
+
+local MicSection = MainTab:CreateSection({
+Name = "Mic"
+})
+
+MicSection:AddButton({
+    Name = 'Infinite Yeld',
+    Callback = function()
+        loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+    end
+})
+
+MicSection:AddButton({
+    Name = 'Dark Dex V3',
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua", true))()
+    end
+})
+
+MicSection:AddButton({
+    Name = 'Remote Spy',
+    Callback = function()
+        loadstring(game:HttpGet("https://github.com/exxtremestuffs/SimpleSpySource/raw/master/SimpleSpy.lua"),true)()
+    end
+})
+end
