8a2e89fd49235238eabca486d27795a0c83567ab
SubnauticaLaserMain <123651736+SubnauticaLaserMain@users.noreply.github.com>
2023-04-28 19:10:59 +0200

    Create WalkSpeed-JumpPower.lua

---
+++ my-Settings/WalkSpeed-JumpPower.lua
@@ -0,0 +1,22 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local window = library.CreateLib("Julians Exploit -- Piggy / Private")
+
+local Main = window:NewTab("Speed & Jump")
+local SpeedSection = Main:NewSection("Speed")
+
+SpeedSection:NewSlider("WalkSpeed", "Private Verison Only For now", 500, 0, function(s)
+	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+
+local JumpSection = Main:NewSection("Jump")
+
+JumpSection:NewSlider("JumpPower", "Private Verison Only For now", 1000, 0, function(s)
+	game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+end)
+
+local ResetSection = Main:NewSection("Reset")
+
+ResetSection:NewButton("Reset WalkSpeed/JumpPower", "Resets W/J", function()
+	game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
+	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+end)
